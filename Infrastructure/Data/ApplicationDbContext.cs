01ddd7ef3d663cb93ac50a3f29bd350c06c9e38e
reza.kharaei <kharaee@gmail.com>
2022-11-16 18:34:38 +0330

    add ioc and data projects in infrastructure folder

---
+++ Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+
+
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Kharaei.Domain;
+
+namespace Kharaei.Infra.Data;
+
+public class ApplicationDbContext: IdentityDbContext<User, Role, int>
+{
+     public ApplicationDbContext(DbContextOptions options)
+            : base(options)
+        {
+
+        }
+} 
