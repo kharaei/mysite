0756047c4852707f223191afbab8ab22a3c99d0f
reza.kharaei <kharaee@gmail.com>
2022-11-16 19:30:03 +0330

    edit solution file

---
+++ Presentation/Api/Controllers/LayoutController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc; 
+
+namespace Kharaei.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class LayoutController : ControllerBase
+{  
+
+    [HttpPost("[action]")]
+    public IActionResult Get()
+    { 
+        return Ok();
+    }
+}
