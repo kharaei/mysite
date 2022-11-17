e14bd9ffca4d9d2a585e34c05ba53f20b2391fc2
reza.kharaei <kharaee@gmail.com>
2022-11-17 09:31:57 +0330

    add dbcontextbuilder

--- Infrastructure/Data/ApplicationDbContext.cs
+++ Infrastructure/Data/ApplicationDbContext.cs
@@ -11,4 +11,18 @@ public class ApplicationDbContext: IdentityDbContext<User, Role, int>
         {
 
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            var entitiesAssembly = typeof(IEntity).Assembly;
+
+            modelBuilder.RegisterAllEntities<IEntity>(entitiesAssembly);
+            modelBuilder.RegisterEntityTypeConfiguration(entitiesAssembly);
+            modelBuilder.AddRestrictDeleteBehaviorConvention();
+            modelBuilder.AddSequentialGuidForIdConvention();
+            modelBuilder.AddPluralizingTableNameConvention();
+        }
+
 } 
