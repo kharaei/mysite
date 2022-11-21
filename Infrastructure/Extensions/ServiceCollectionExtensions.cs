08ef1cd96481aa54b29452f0e8e7a9572d5d9a4c
reza.kharaei <kharaee@gmail.com>
2022-11-21 13:49:50 +0330

    change servicecollection class

--- Infrastructure/Extensions/ServiceCollectionExtensions.cs
+++ Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -1,5 +1,7 @@
+
 using System.Text;
 using System.Net;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 //using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.IdentityModel.Tokens; 
@@ -7,6 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Kharaei.Application;
+using Kharaei.Domain;
 using System.Security.Claims;
 
 namespace Kharaei.Infra;
@@ -24,7 +27,33 @@ public static class ServiceCollectionExtensions
         });
     }
 
-    
+    public static void AddCustomIdentity(this IServiceCollection services, IdentitySettings settings)
+    {
+        services.AddIdentity<User, Role>(identityOptions =>
+        {
+            //Password Settings
+            identityOptions.Password.RequireDigit = settings.PasswordRequireDigit;
+            identityOptions.Password.RequiredLength = settings.PasswordRequiredLength;
+            identityOptions.Password.RequireNonAlphanumeric = settings.PasswordRequireNonAlphanumic; //#@!
+            identityOptions.Password.RequireUppercase = settings.PasswordRequireUppercase;
+            identityOptions.Password.RequireLowercase = settings.PasswordRequireLowercase;
+
+            //UserName Settings
+            identityOptions.User.RequireUniqueEmail = settings.RequireUniqueEmail;
+
+            //Singin Settings
+            identityOptions.SignIn.RequireConfirmedEmail = false;
+            identityOptions.SignIn.RequireConfirmedPhoneNumber = false;
+
+            //Lockout Settings
+            //identityOptions.Lockout.MaxFailedAccessAttempts = 5;
+            //identityOptions.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+            //identityOptions.Lockout.AllowedForNewUsers = false;
+        })
+        .AddEntityFrameworkStores<KharaeiDbContext>()
+        .AddDefaultTokenProviders();
+    }
+   
     public static void AddJwtAuthentication(this IServiceCollection services, JwtSettings jwtSettings)
     {
         services.AddAuthentication(options =>
--- Presentation/Api/Program.cs
+++ Presentation/Api/Program.cs
@@ -4,11 +4,10 @@ using Kharaei.Application;
 var builder = WebApplication.CreateBuilder(args);
  
 // Add services to the container. 
-builder.Services.AddDbContext(builder.Configuration);
-var _siteSetting = builder.Configuration.GetSection(nameof(SiteSettings)).Get<SiteSettings>();
-builder.Services.AddCustomIdentity(_siteSetting.IdentitySettings);
-builder.Services.AddJwtAuthentication(_siteSetting.JwtSettings);
-builder.Services.AddCustomApiVersioning();
+builder.Services.AddDbContext(builder.Configuration); 
+builder.Services.AddCustomIdentity(builder.Configuration.GetSection(nameof(SiteSettings)).Get<SiteSettings>().IdentitySettings);
+//builder.Services.AddJwtAuthentication(builder.Configuration.GetSection(nameof(SiteSettings)).Get<SiteSettings>().JwtSettings);
+//builder.Services.AddCustomApiVersioning();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -16,7 +15,6 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build(); 
- 
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -26,9 +24,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
